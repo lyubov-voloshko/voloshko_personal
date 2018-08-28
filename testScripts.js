fefb8d81709e6fad363907d7807c89a78e8680ca
Lyubov Mac <lyubov@voloshko.com>
2018-08-28 18:00:23 +0300

    addition to referrer test

--- testScripts.js
+++ testScripts.js
@@ -1,13 +1,20 @@
-var fromWhere = document.referrer;
+var fromWhere;
+var referrerOutput;
+var referrerCookie;
 
-var referrerOutput = document.getElementById('pageReferrer');
-var referrerCookie = document.getElementById('pageCookie');
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    fromWhere = document.referrer;
+    referrerOutput = document.getElementById('pageReferrer');
+    referrerCookie = document.getElementById('pageCookie');
+    referrerOutput.textContent = fromWhere;
 
-referrerOutput.textContent = fromWhere;
+    document.cookie = "username=John Doe";
+    document.cookie = "referrer=" + fromWhere;
 
-document.cookie = "username=John Doe";
-document.cookie = "referrer=" + fromWhere;
+    var fromCookie = document.cookie;
 
-var fromCookie = document.cookie;
+    referrerCookie.textContent = fromCookie;
+});
 
-referrerCookie.textContent = fromCookie;
+
