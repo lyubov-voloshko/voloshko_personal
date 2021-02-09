12bb436e1cfa6dfe491982c48c761f86ddf44fcd
Lyubov Voloshko <lyubov@voloshko.com>
2021-02-09 17:44:10 +0200

    timeline page

---
+++ timeline/cards.js
@@ -0,0 +1,102 @@
+export default [
+    {
+        id: 1,
+        title: "Invention of the wheel",
+        year: -3500
+    },
+    {
+        id: 2,
+        title: "Modern alphabet invented",
+        year: -1600
+    },
+    {
+        id: 3,
+        title: "Foundation of Rome",
+        year: -753
+    },
+    {
+        id: 4,
+        title: "Birth of Buddha",
+        year: -486
+    },
+    {
+        id: 5,
+        title: "First university founded in Bologna",
+        year: 1088
+    },
+    {
+        id: 6,
+        title: "First book printed with moveable type",
+        year: 1455
+    },
+    {
+        id: 7,
+        title: "Christopher Columbus discovers the New World",
+        year: 1492
+    },
+    {
+        id: 8,
+        title: "William Shakespeare is born",
+        year: 1564
+    },
+    {
+        id: 9,
+        title: "Isaac Newton publishes Principia Mathematica",
+        year: 1687
+    },
+    {
+        id: 10,
+        title: "American Declaration of Independence",
+        year: 1776
+    },
+    {
+        id: 11,
+        title: "French Revolution",
+        year: 1789
+    },
+    {
+        id: 12,
+        title: "Rocket steam locomotive built",
+        year: 1825
+    },
+    {
+        id: 13,
+        title: "Publication of Darwin’s The Origin of Species",
+        year: 1859
+    },
+    {
+        id: 14,
+        title: "Einstein’s theory of special relativity published",
+        year: 1905
+    },
+    {
+        id: 15,
+        title: "Invention of the silicon chip",
+        year: 1959
+    },
+    {
+        id: 16,
+        title: "First contraceptive pill made available for women",
+        year: 1960
+    },
+    {
+        id: 17,
+        title: "Yuri Gagarin of USSR becomes the first spaceman",
+        year: 1961
+    },
+    {
+        id: 18,
+        title: "Death of Sir Winston Churchill",
+        year: 1965
+    },
+    {
+        id: 19,
+        title: "Death of Sir Winston Churchill",
+        year: 1965
+    },
+    {
+        id: 20,
+        title: "Chernobyl disaster",
+        year: 1986
+    }
+]
---
+++ timeline/index.js
@@ -0,0 +1,86 @@
+import cards from "./cards.js";
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        cards: shuffle(cards),
+        selectedCard: {},
+        selectedCardIndex: Number,
+        playedCards: [],
+        playersCards: [],
+        packCards: []
+    },
+    methods: {
+        selectCard: function(card, index) {
+            this.selectedCard = card;
+            this.selectedCardIndex = index;
+        },
+        removeCardFromPlayersCards() {
+            let currentPlayersCard = [...this.playersCards];
+            currentPlayersCard.splice(this.selectedCardIndex, 1)
+            this.playersCards = currentPlayersCard;
+        },
+        addCardFromPlayersCards() {
+            if (this.packCards.length) {
+                let currentPlayersCard = [...this.playersCards];
+                currentPlayersCard.push(this.packCards[0])
+                this.playersCards = currentPlayersCard;
+                this.packCards.splice(0, 1);
+            }
+        },
+        addCardIntoPlayedCards(index) {
+            let currentPlayedCard = [...this.playedCards];
+            currentPlayedCard.splice(index, 0, this.selectedCard)
+            this.playedCards = currentPlayedCard;
+        },
+        putCard: function(card, index) {
+            if (index === -1 && card.year > this.selectedCard.year) {
+                this.addCardIntoPlayedCards(index + 1);
+                this.removeCardFromPlayersCards();
+            } else if (index === this.playedCards.length - 1 && this.playedCards[index].year < this.selectedCard.year) {
+                this.addCardIntoPlayedCards(index + 1);
+                this.removeCardFromPlayersCards();
+            } else if (index !== -1 && this.playedCards[index].year < this.selectedCard.year && this.playedCards[index+1].year > this.selectedCard.year) {
+                this.addCardIntoPlayedCards(index + 1);
+                this.removeCardFromPlayersCards();
+            } else {
+                console.log('error');
+                this.removeCardFromPlayersCards();
+                this.addCardFromPlayersCards();
+            }
+        }
+    },
+    beforeMount() {
+        this.playedCards = this.cards.slice(0, 1);
+        this.playersCards = this.cards.slice(1, 7);
+        this.packCards = this.cards.slice(7, 20);
+    }
+})
+
+function shuffle(array) {
+    const length = array == null ? 0 : array.length
+    if (!length) {
+      return []
+    }
+    let index = -1
+    const lastIndex = length - 1
+    const result = copyArray(array)
+    while (++index < length) {
+      const rand = index + Math.floor(Math.random() * (lastIndex - index + 1))
+      const value = result[rand]
+      result[rand] = result[index]
+      result[index] = value
+    }
+    return result
+}
+
+function copyArray(source, array) {
+    let index = -1
+    const length = source.length
+
+    array || (array = new Array(length))
+    while (++index < length) {
+      array[index] = source[index]
+    }
+    return array
+  }
